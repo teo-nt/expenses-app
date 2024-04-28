e08892fe4b119a291436b720bf7698144c8ee152
Teo <thod_ntonas@hotmail.com>
2024-04-29 02:00:24 +0300

    Add class for database connection

---
+++ Services/DBHelper/DBHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+
+namespace ExpensesManagementApp.Services.DBHelper
+{
+    public static class DBHelper
+    {
+        private static SqlConnection? _connection;
+
+        public static SqlConnection? GetConnection()
+        {
+            var configurationBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            var configuration = configurationBuilder.Build();
+            string url = configuration.GetConnectionString("DefaultConnection")!;
+
+            try
+            {
+                _connection = new SqlConnection(url);
+                return _connection;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return null;
+            }
+        }
+    }
+}
