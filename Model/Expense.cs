96cb744c1f8d21aa5483a5d6bff3192093780efc
Teo <thod_ntonas@hotmail.com>
2024-04-29 01:44:59 +0300

    Create the model of app

---
+++ Model/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpensesManagementApp.Model
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Amount { get; set; }
+        public string? Category { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
